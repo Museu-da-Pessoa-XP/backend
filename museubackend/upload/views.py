1778042cc5c0c22aa8170d1f43177ab84e19390f
tutss <artur_santos@usp.br>
2020-10-10 18:12:18 -0300

    Started creating upload view set( maybe using a function, as the tutorial used, would be easier)

--- museubackend/upload/views.py
+++ museubackend/upload/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from museubackend.upload.models import Upload
+from museubackend.museubackend.storage import LocalStorage
+
+
+class UploadViewSet(viewsets.ModelViewSet):
+    queryset = Upload.uploaded_at
+    serializer_class = None
+    permission_classes = [permissions.IsAuthenticated]
+
+    fs = LocalStorage()
+    filename = fs.save(image_file.name, image_file)
+    image_url = fs.url(filename)

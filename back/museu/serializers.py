7f036d9e9549efdac35142a3a2af3bf2d6098da5
Antonio <anttoniocl@yahoo.com>
2020-10-13 00:33:06 -0300

    Create model, views, serializers and endpoint for user, historia and upload

---
+++ back/museu/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from museu.models import User, Historia, Upload
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['nome', 'email']
+
+class HistoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Historia
+        fields = ['nome', 'usuario']
+
+class UploadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Upload
+        fields = ['uploaded_at', 'file']
+

fcfe5a47d22aeebdf268641c01bb5c738b389269
Antonio <anttoniocl@yahoo.com>
2020-10-13 05:20:25 -0300

    Add changes to views.py and tests: testcase for User model and script to test insertion

--- back/museu/tests.py
+++ back/museu/tests.py
@@ -1,3 +1,42 @@
+import random
+import string
+
 from django.test import TestCase
 
-# Create your tests here.
+from back.museu.models import User
+
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
+
+
+class UserTestCase(TestCase):
+
+    def __init__(self):
+        self.user_names = []
+        self.emails = []
+
+    def setUp(self):
+        string_length = random.randint(1, User.nome.max_length)
+        num_string = random.randint(10, 20)
+        email_suffix = "@email.com"
+        self.user_names = [get_random_string(string_length) for i in range(num_string)]
+        self.emails = [user_name+email_suffix for user_name in self.user_names]
+        for user_name, email in zip(self.user_names, self.emails):
+            User.objects.create(nome=user_name, email=email)
+
+    def test_user_model(self):
+        users = []
+        for user_name, email in zip(self.user_names, self.emails):
+            users.append(User.objects.create(nome=user_name, email=email))
+        for i in range(users):
+            self.assertEqual(users[i].nome, self.user_names[i])
+            self.assertEqual(users[i].email, self.emails[i])
+
+
+
+
+
+

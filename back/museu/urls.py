87ff851cf84d2d7ae405fa04d601d149cce4d7d6
Antonio <anttoniocl@yahoo.com>
2020-10-13 05:20:25 -0300

    Add changes to views.py and tests: testcase for User model and script to test insertion

--- back/museu/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from museu import views
-
-urlpatterns = [
-    path('user/', views.UserDetails.as_view()),
-    path('historia/', views.HistoriaDetails.as_view()),
-    path('upload/', views.UploadDetails.as_view()),
-]

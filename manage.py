be444839c9c1784477b9ed4e8359ef1a9519bbcb
Antonio <anttoniocl@yahoo.com>
2020-10-17 15:54:29 -0300

    Create project using the right Django version

--- manage.py
+++ manage.py
@@ -5,10 +5,7 @@ import sys
 
 
 def main():
-<<<<<<< HEAD:manage.py
     """Run administrative tasks."""
-=======
->>>>>>> Write name of variables and directories in English:back/manage.py
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
         from django.core.management import execute_from_command_line

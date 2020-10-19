c32751dedf0620d7f7c92f9ad88608259019c19e
tutss <artur_santos@usp.br>
2020-10-19 19:45:19 -0300

    Complete tests for API and models: Test are now complete, testing POST and GET methods. Also, we tested the models, just to make sure everything was working properly.

--- museu/tests/test_models.py
+++ museu/tests/test_models.py
@@ -3,7 +3,7 @@ import random
 import string
 
 from django.test import TestCase
-from museu.models import User, USER_MAX_LENGTH
+from museu.models import User, Historia, USER_MAX_LENGTH
 
 
 def get_random_string(length):
@@ -12,7 +12,7 @@ def get_random_string(length):
     return result_str
 
 
-class UserTestCase(TestCase):
+class ModelsTestCase(TestCase):
 
     def setUp(self):
         string_length = random.randint(1, USER_MAX_LENGTH)
@@ -20,21 +20,31 @@ class UserTestCase(TestCase):
         email_suffix = "@email.com"
         self.user_names = [get_random_string(string_length) for i in range(num_string)]
         self.emails = [user_name+email_suffix for user_name in self.user_names]
-        for user_name, email in zip(self.user_names, self.emails):
-            User.objects.create(name=user_name, email=email)
+        self.historia_names = [get_random_string(string_length) for i in range(num_string)]
+        self.locations = [get_random_string(string_length) for i in range(num_string)]
+        self.users = []
+        for username, email, historia_name, location in zip(self.user_names, self.emails,
+                                                            self.historia_names, self.locations):
+            User.objects.create(name=username, email=email)
+            user = User.objects.get(name=username)
+            self.users.append(user)
+            Historia.objects.create(name=historia_name, location=location, user=user)
 
     def test_user_model(self):
+        users_created = User.objects.all()
         users = []
         for user_name, email in zip(self.user_names, self.emails):
             users.append(User.objects.create(name=user_name, email=email))
         for i in range(len(users)):
-            self.assertEqual(users[i].name, self.user_names[i])
-            self.assertEqual(users[i].email, self.emails[i])
-
-class HistoriaTestCase(TestCase):
-
-    def setUp(self):
-        pass
+            self.assertEqual(users[i].name, users_created[i].name)
+            self.assertEqual(users[i].email, users_created[i].email)
 
     def test_historia_model(self):
-        pass
+        historias_created = Historia.objects.all()
+        historias = []
+        for historia_name, location, user in zip(self.historia_names, self.locations, self.users):
+            historias.append(Historia.objects.create(name=historia_name, location=location, user=user))
+        for i in range(len(historias)):
+            self.assertEqual(historias[i].name, historias_created[i].name)
+            self.assertEqual(historias[i].location, historias_created[i].location)
+            self.assertEqual(historias[i].user, historias_created[i].user)

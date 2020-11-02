3bd4c5669b45d0a9767eddad7aef2df56873fc0f
RenanSchwyz <renancostalaiz@gmail.com>
2020-11-02 17:05:22 -0300

    Saving front data into PostgreSQL

--- backend/settings.py
+++ backend/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'museu.apps.MuseuConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -81,13 +82,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
 
 DATABASES = {
     'default': {

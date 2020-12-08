0aa5b01bce9b4ad068a5165c91b527c508512019
tutss <artur_santos@usp.br>
2020-12-08 19:19:50 -0300

    Fix typos
    Co-authored-by: Renan Costa Laiz <renancostalaiz@gmail.com>

--- museu/validators.py
+++ museu/validators.py
@@ -1,8 +1,13 @@
+
 from django.core.exceptions import ValidationError
 
 ALLOWED_TYPES = ['audio', 'text', 'video']
 
 
 def validate_historia_type(value):
+    """
+    Validates the Historia types
+    """
     if value not in ALLOWED_TYPES:
-        raise ValidationError('%s is not an valid Historia type' % value, params={'value': value})
+        raise ValidationError('%s is not an valid Historia type'
+                              % value, params={'value': value})
